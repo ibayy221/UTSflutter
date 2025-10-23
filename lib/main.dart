1b472d6ffda0cc6bc6a380b2b87085911f6b7d75
Bayu7d11 <Bayu07454@gmail.com>
2025-10-23 20:25:03 +0700

    teks lks

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Image.asset('gambar/Logo.png', width: 150)),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('gambar/Logo.png', width: 90),
+              const SizedBox(width: 20),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'LKS',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3A5BA0),
+                    ),
+                  ),
+                  Text(
+                    'MART',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3A5BA0),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
